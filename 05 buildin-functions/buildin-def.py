4a8ecbc9016692d84805719cea10cefcf9ec437e
Dmitriy Grigorev <dngrigorev82@yandex.ru>
2024-06-25 16:59:56 +0700

    buildin-functions

---
+++ 05 buildin-functions/buildin-def.py
@@ -0,0 +1,16 @@
+# me_fn Имя функции    # (a, b)Параметры функции
+def my_fn(a, b):
+    a = a + 1   # Тело
+    c = a + b   # функции
+    return c    # Возврат результата функции
+
+print (my_fn(5,10))
+
+
+print(print)
+
+# name = input("Enter your name: ")
+# print (name)
+
+name = 'Fibo'
+print(dir(name))
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# develop
 # Уроки по Python
